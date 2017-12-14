7095f18d95fdbbb92a4bb9421d1752dac6c49c95
Vincent Scotti <scottivincentj@gmail.com>
2017-12-14 03:06:22 +0000

    End of cp-8

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   
   resources :wikis
   resources :charges, only: [:new, :create]
+  post 'charges/downgrade', :to => 'charges#downgrade'
   devise_for :users
   get 'welcome/about'
   root 'welcome#index'
